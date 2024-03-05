f7993abce87a42015bbb87fa169ce9561f462f1a
Galaktikshapes <tmakgakga@gmail.com>
2024-03-05 14:26:23 +0200

    Bash Programmes

---
+++ five.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Program to run my other four programs
+
+
+# Execute the questionnaire.sh script
+./questionnaire.sh
+
+# Execute the countdown.sh script with an argument of 3
+./countdown.sh 3
+
+# Execute the bingo.sh script
+./bingo.sh
+
+# Execute the fortune.sh script
+./fortune.sh
